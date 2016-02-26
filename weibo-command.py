758367888ed09dd0f41ff1e9634444dee2fd1f6a
lexkde <lexkde@lexkde-HP-ENVY-15-Notebook-PC>
2016-02-26 20:31:18 +0800

    对获取token使用分步的操作，有对应的函数get_url, update_token

--- weibo-command.py
+++ weibo-command.py
@@ -14,6 +14,33 @@ SOME_CODE = 1
 USERID='supery_ky@sina.com'
 PASSWD='153381523'
 
+def get_url():
+    '''获取用户第一条评论，当第一条微博改变时，进行命令执行，并在微博评论处返回执行信息'''
+
+    client=APIClient(APP_KEY,APP_SECRET,CALLBACK_URL)
+    url = client.get_authorize_url() # redirect the user to 'url'
+    print url
+    
+def update_token():
+    '''获取用户第一条评论，当第一条微博改变时，进行命令执行，并在微博评论处返回执行信息'''
+
+    client=APIClient(APP_KEY,APP_SECRET,CALLBACK_URL)
+    url = client.get_authorize_url() # redirect the user to 'url'
+    #获得url，进行浏览器调用，手动的，呵呵！
+    #获得code
+    code = 'd624fa7b15d139c453023f4b70731e79' #需要获得一个新的code
+    
+    r=client.request_access_token(code) #这个调用对于一个code只能执行一次
+    #但是返回的r.access_token都是一样的，可以将token记录下来，隔一段时间再调用一次
+    access_token = r.access_token # 新浪返回的token，类似abc123xyz456
+    #如果过期了，需要将上面的几步再执行一次获得新的access_token，并手动替换
+    
+    print access_token
+    expires_in = r.expires_in # token过期的UNIX时间：http://zh.wikipedia.org/wiki/UNIX%E6%97%B6%E9%97%B4
+# TODO: 在此可保存access token
+    print expires_in
+    
+
 def main():
     '''获取用户第一条评论，当第一条微博改变时，进行命令执行，并在微博评论处返回执行信息'''
 
@@ -22,19 +49,14 @@ def main():
     print url
     #获得url，进行浏览器调用，手动的，呵呵！
     #获得code
-    code = 'd624fa7b15d139c453023f4b70731e79'
+    #code = 'd624fa7b15d139c453023f4b70731e79'
     #r=client.request_access_token(code) #这个调用对于一个code只能执行一次
     #但是返回的r.access_token都是一样的，可以将token记录下来，隔一段时间再调用一次
     #access_token = r.access_token # 新浪返回的token，类似abc123xyz456
     #如果过期了，需要将上面的几步再执行一次获得新的access_token，并手动替换
-    access_token = '2.00F9dUnB0kKJTs9164339a26ROiY8B'
-    
-    #print access_token
-    #expires_in = r.expires_in # token过期的UNIX时间：http://zh.wikipedia.org/wiki/UNIX%E6%97%B6%E9%97%B4
-# TODO: 在此可保存access token
-    #print expires_in
-    expires_in=1614153145
-    client.set_access_token(access_token,expires_in)
+    access_token = '2.00F9dUnB0kKJTs9164339a26ROiY8B' #需要被更新
+    expires_in=1614153145 #需要被更新
+    client.set_access_token(access_token,expires_in) #保存这两个值
 
     UID = client.account.get_uid.get()['uid'] #获取用户UID
     print 'user id is:',UID
